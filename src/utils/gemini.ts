0402c27057ef9359b068678915357a1bdecf1526
patiladitya77 <adpatil587@gmail.com>
2025-06-30 00:01:37 +0530

    securing keys using .env

--- src/utils/gemini.ts
+++ src/utils/gemini.ts
@@ -1,7 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
-import { API_KEY } from "./geminiConstants";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({ apiKey: API_KEY });
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_KEY });
 
 export default ai;
